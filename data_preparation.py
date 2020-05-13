d01f530533ddda71fbf9ce197cec9ff860d862cf
rahulmoorthy19 <rahulmoorthy9.6@gmail.com>
2020-05-13 09:51:33 +0530

    completed voc annotation format

--- data_preparation.py
+++ data_preparation.py
@@ -9,12 +9,12 @@ def train_test_annotation_convert(txt_name):
     for i in annotation_file.readlines():
         file_name=i.split()[0]
         number_of_objects=int(i.split()[1])
-        anob_x,anob_y,anob_width,anob_height,anob_brand=[],[],[],[],[]
+        anob_x_min,anob_y_min,anob_x_max,anob_y_max,anob_brand=[],[],[],[],[]
         for j in range(number_of_objects):
-            anob_x.append(i.split()[5*j+2])
-            anob_y.append(i.split()[5*j+3])
-            anob_width.append(i.split()[5*j+4])
-            anob_height.append(i.split()[5*j+5])
+            anob_x_min.append(i.split()[5*j+2])
+            anob_y_min.append(i.split()[5*j+3])
+            anob_x_max.append(str(int(i.split()[5*j+2])+int(i.split()[5*j+4])))
+            anob_y_max.append(str(int(i.split()[5*j+3])+int(i.split()[5*j+5])))
             anob_brand.append(i.split()[5*j+6])
         data_dist=""
         width=0
@@ -30,7 +30,7 @@ def train_test_annotation_convert(txt_name):
         annotation_per_file = open("A:\\Infilect_project\\Product_detector\\Dataset\\Annotations\\"+data_dist+"\\"+file_name.split(".")[0]+".txt", "a")
         annotation_per_file.write(str(width)+" "+str(height)+"\n")
         for j in range(len(anob_brand)):
-            annotation_per_file.write(anob_brand[j]+" "+anob_x[j]+" "+anob_y[j]+" "+anob_width[j]+" "+anob_height[j]+"\n")
+            annotation_per_file.write(anob_brand[j]+" "+anob_x_min[j]+" "+anob_y_min[j]+" "+anob_x_max[j]+" "+anob_y_max[j]+"\n")
         annotation_per_file.close()
     annotation_file.close()
     print("Train Test Distribution of Annotation Files done!")
@@ -59,14 +59,14 @@ def annotation_load(ann_dir, img_dir, labels=[]):
                 else:
                     img['object'] += [obj]
                 obj['xmin'] = int(round(float(j.split()[1])))
-                obj['xmax'] = int(round(float(j.split()[2])))
-                obj['width'] = int(round(float(j.split()[3])))
-                obj['height'] = int(round(float(j.split()[4])))
+                obj['ymin'] = int(round(float(j.split()[2])))
+                obj['xmax'] = int(round(float(j.split()[3])))
+                obj['ymax'] = int(round(float(j.split()[4])))
 
             if len(img['object']) > 0:
                 all_insts += [img]
             k=k+1
     return all_insts,seen_labels
 
-#train_test_annotation_convert("A:\\Infilect_project\\Product_detector\\Dataset\\annotation.txt")
-annotation_load("A:\\Infilect_project\\Product_detector\\Dataset\\Annotations\\train\\","A:\\Infilect_project\\Product_detector\\Dataset\\Annotations\\test\\")
+# train_test_annotation_convert("A:\\Infilect_project\\Product_detector\\Dataset\\annotation.txt")
+# annotation_load("A:\\Infilect_project\\Product_detector\\Dataset\\Annotations\\train\\","A:\\Infilect_project\\Product_detector\\Dataset\\Annotations\\test\\")
